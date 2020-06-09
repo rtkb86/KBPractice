23cc0f2ef77c3553a0ca13c4ff7a2a3ef810d285
rtkb86 <60709046+rtkb86@users.noreply.github.com>
2020-06-09 23:45:39 +0300

    backend

---
+++ backend/Adminpanel.php
@@ -0,0 +1,135 @@
+<?php session_start();
+include 'bd.php';
+include 'bdz.php';
+?>
+<?if($role == 2):?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title></title>
+    <link type="text/css" rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link type="text/css" rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Adminpanel.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Log%20in.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Main.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Search.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Sign%20up.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div class="header-dark">
+        <nav class="navbar navbar-dark navbar-expand-lg navigation-clean-search" id="nbar_m">
+            <div class="container"><a id="logo" href="Main.php"><img id="logo" src="assets/img/logo.png" style="width: 80px;"></a>
+                <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item" role="presentation"></li>
+                    </ul>
+                    <form class="form-inline mr-auto" target="_self">
+                        <div class="form-group"><label for="search-field"></label></div>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <p><br><div class="form-group pull-right">
+    
+</div>
+<span class="counter pull-right"></span>
+<table class="table table-hover table-bordered results">
+  <thead>
+    <tr>
+      <th id="tbltext">#</th>
+      <th id="tbltext" class="col-md-5 col-xs-5">Название документа</th>
+      <th id="tbltext" class="col-md-4 col-xs-4">Дата загрузки</th>
+      <th id="tbltext" class="col-md-3 col-xs-3">Статус</th>
+    </tr>
+    <tr class="warning no-result">
+      <td id="tbltext" colspan="4"><i class="fa fa-warning"></i> Нет результатов</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th id="tbltext" scope="row">1</th>
+      <td id="tbltext" >Документ №1     
+          <a id="abut" class="btn btn-light action-button" role="button">Изменить</a>
+          <a id="abut" class="btn btn-light action-button" role="button">Удалить</a></td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">2</th>
+      <td id="tbltext" >Документ №2
+          <a id="abut" class="btn btn-light action-button" role="button">Изменить</a>
+        <a id="abut" class="btn btn-light action-button" role="button">Удалить</a>
+        <a id="abut" class="btn btn-light action-button" role="button">Утвердить</a></td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">3</th>
+      <td id="tbltext" >Документ №3 
+       <a id="abut" class="btn btn-light action-button" role="button">Изменить</a>
+        <a id="abut" class="btn btn-light action-button" role="button">Удалить</a>
+          <a id="abut" class="btn btn-light action-button" role="button">Утвердить</a> </td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">4</th>
+      <td id="tbltext" >Документ №4
+        <a id="abut" class="btn btn-light action-button" role="button">Изменить</a>
+        <a id="abut" class="btn btn-light action-button" role="button">Удалить</a>
+          <a id="abut" class="btn btn-light action-button" role="button">Утвердить</a></td>
+      <td id="tbltext" >24.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+  </tbody>
+</table>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Table-with-search.js"></script>
+<?php
+$result = mysqli_query($bd, "select `username`, `id`, `role_id` from user join user_role on user.id = user_role.user_id"); //извлекаем из базы все данные о пользователе с введенным логином
+$myrow = mysqli_fetch_all($result);
+$id = $myrow['id'];
+$role = $myrow['role_id']; 
+echo '<table class="table table-hover table-bordered results">'."\n";
+echo '<tr align="center">'."\n";
+    echo '<th>Логин</th>'."\n";
+    echo '<th>Айди</th>'."\n";
+    echo '<th>Роль</th>'."\n";
+echo '</tr>';
+ 
+foreach ($myrow as $val) {
+    echo '<tr>'."\n";
+if ($val[2] == 2){
+    $val[2]='Администратор';
+} elseif ($val[2] == 1){
+    $val[2]='Пользователь';
+} 
+    foreach ($val as $value) { 
+        if ($value == 'Пользователь'){
+            echo '<td>'.$value."\n";
+            echo '<form action="change.php" method="post"><input name='."$val[1]".' id="abut" class="btn btn-light action-button" value="Повысить" type="submit"></td></form>'."\n";
+        } else {
+    
+        echo '<td>'.$value.'</td>'."\n";}
+    }
+    echo '</tr>'."\n";
+
+}
+echo '</table>'."\n"; 
+?>
+</body>
+
+</html>
+<?else:
+  echo 'У тебя нет доступа,';
+  ?>
+  <a href="Main.php">Авторизуйся</a>
+<?endif?>
---
+++ backend/Main.php
@@ -0,0 +1,110 @@
+<?php session_start();
+include 'bd.php';
+include 'bdz.php';
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title></title>
+    <link type="text/css" rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link type="text/css" rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Adminpanel.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Log%20in.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Main.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Search.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/Sign%20up.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div>
+        <div class="header-dark">
+            <nav class="navbar navbar-dark navbar-expand-lg navigation-clean-search" id="nbar_m">
+                <div class="container">
+                  <a id="logo" href="Main.php">
+                    <img id="logo" src="assets/img/logo.png" style="width: 80px;">
+                  </a>
+                  <input type="search" id="search-field" class="search-field" name="search">
+                  <a id="search1" class="btn btn-light action-button" href="Search.html">
+                    <i class="fa fa-search"></i>&nbsp;
+                  </a>
+                  <?if ($role == 2):?>
+                  <a id="admin" class="btn btn-light action-button" href="Adminpanel.php">К панели администратора</a>
+                  <?endif?>
+                  <a class="btn btn-light action-button" href="Upload.php">Загрузить файл</a>
+                  <div class="collapse navbar-collapse" id="navcol-1">
+                    <ul class="nav navbar-nav">
+                      <li class="nav-item" role="presentation"></li>
+                    </ul>
+                    <form class="form-inline mr-auto" target="_self">
+                      <div class="form-group"><label for="search-field"></label></div>
+                    </form>
+                  </div>
+                    <?if(empty($id)):?>
+                      <a class="btn btn-light action-button" role="button" href="Log%20in.html">Вход</a>
+                      <a class="btn btn-light action-button" role="button" href="Sign%20up.html">Регистрация</a>
+                    <?else:?>
+                      <a class="btn btn-light action-button" role="button" href="#" >Аккаунт</a>
+                      <form action="exit.php" method="post">
+                      <input class="btn btn-light action-button" type="submit" value="Выйти">
+                      </form>
+                    <?endif?>  
+                    <?php//echo $_SESSION['id']// нада для отладки =)?>           
+            </nav>
+            <p><br><div class="form-group pull-right">
+    
+</div>
+<span class="counter pull-right"></span>
+<table class="table table-hover table-bordered results">
+  <thead>
+    <tr>
+      <th id="tbltext">#</th>
+      <th id="tbltext" class="col-md-5 col-xs-5">Название документа</th>
+      <th id="tbltext" class="col-md-4 col-xs-4">Дата загрузки</th>
+      <th id="tbltext" class="col-md-3 col-xs-3">Статус</th>
+    </tr>
+    <tr class="warning no-result">
+      <td id="tbltext" colspan="4"><i class="fa fa-warning"></i> Нет результатов</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th id="tbltext" scope="row">1</th>
+      <td id="tbltext" >Документ №1</td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">2</th>
+      <td id="tbltext" >Документ №2</td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">3</th>
+      <td id="tbltext" >Документ №3</td>
+      <td id="tbltext" >25.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+    <tr>
+      <th id="tbltext" scope="row">4</th>
+      <td id="tbltext" >Документ №4</td>
+      <td id="tbltext" >24.05.2020</td>
+      <td id="tbltext" >Не подтвержден</td>
+    </tr>
+  </tbody>
+</table></p>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/Table-with-search.js"></script>
+</body>
+
+</html>
